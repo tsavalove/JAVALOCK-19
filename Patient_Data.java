27f4dd4590f124fa5c32afed2f092632e20d2bbd
Danai Archontouli <72792727+DanaeAr@users.noreply.github.com>
2021-01-25 18:33:49 +0200

    Delete Patient_Data.java

--- Patient_Data.java
+++
@@ -1,9 +0,0 @@
-package CovidTrace;
-
-public class Patient_Data extends Patient{
-	
-	public Patient_Data(String name, String surname, String dob, String adress, int phonenum
-			) {
-		super(name, surname, dob, adress, phonenum, adress);
-
-	}}
